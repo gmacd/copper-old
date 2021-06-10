f9d44d0805a931bb0549106ead1d8cfddd33b897
Graham MacDonald <grahamamacdonald@gmail.com>
2021-06-10 22:58:01 +0100

    just use zig to build exes, use scripts to build image

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#rm -rf copper.img boot/
+
+mkdir -p zig-out/boot/aarch64/sys
+mkdir -p zig-out/boot/x86_64/sys
+
+cp zig-out/copper.x86_64 zig-out/boot/x86_64/sys/copperkernel
+cp zig-out/copper.aarch64 zig-out/boot/aarch64/sys/copperkernel
+cp bootboot/config zig-out/config
+
+cd zig-out && ../bootboot/mkbootimg ../bootboot/copper-mkbootimg.json copper.img
+
+#rm -rf boot/
+
+# qemu x86 bios
+# qemu-system-x86_64 -drive file=zig-out/copper.img,format=raw -serial stdio
+# qemu x86 efi
+# qemu-system-x86_64 -bios /usr/share/qemu/OVMF.fd -m 64 -drive file=zig-out/copper.img,format=raw -serial stdio
+# qemu x86 linux
+# qemu-system-x86_64 -kernel bootboot.bin -drive file=zig-out/copper.img,format=raw -serial stdio
+# qemu aarch64
+# qemu-system-aarch64 -M raspi3 -kernel bootboot-rpi.img -drive file=zig-out/copper.img,if=sd,format=raw -serial stdio
--- build.zig
+++ build.zig
@@ -1,47 +1,69 @@
-const Array = @import("std").ArrayList;
-const Builder = @import("std").build.Builder;
 const builtin = @import("builtin");
-const CrossTarget = @import("std").zig.CrossTarget;
-const Arch = @import("std").Target.Cpu.Arch;
-const Step = @import("std").build.Step;
+const std = @import("std");
+const Array = std.ArrayList;
+const Builder = std.build.Builder;
+const CrossTarget = std.zig.CrossTarget;
+const Arch = std.Target.Cpu.Arch;
+const Step = std.build.Step;
+const fs = std.fs;
+const print = @import("std").debug.print;
 
-const buildDir = "build";
+const buildDir = "zig-out";
 
 pub fn build(b: *Builder) void {
     const kernelStepX86 = buildKernel(b, Arch.x86_64);
     const kernelStepAarch64 = buildKernel(b, Arch.aarch64);
     _ = kernelStepX86;
     _ = kernelStepAarch64;
-    //const kernelOutputPath = buildKernelImageX86(b, kernelStep);
+    //const kernelOutputPath = buildKernelImage(b, kernelStepX86, kernelStepAarch64);
     //qemuStep(b, kernelOutputPath);
 }
 
+/// Build the kernel for the given architecture
 fn buildKernel(b: *Builder, comptime arch: Arch) *Step {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const buildMode = b.standardReleaseOptions();
     const target = CrossTarget{ .cpu_arch = arch, .os_tag = .freestanding, .abi = .none };
 
+    // Build kernel
     const kernel = b.addExecutable("copper." ++ @tagName(arch), "src/main.zig");
     kernel.setBuildMode(buildMode);
     kernel.setTarget(target);
     kernel.setLinkerScriptPath("src/linker.ld");
     kernel.setOutputDir(buildDir);
+
     b.default_step.dependOn(&kernel.step);
+
     return &kernel.step;
 }
 
-// fn buildKernelImageX86(b: *Builder, kernelStep: *Step) []const u8 {
+/// Build a bootboot-based image containing both x86_64 and aarch64 kernels
+// fn buildKernelImage(b: *Builder, kernelStepX86: *Step, kernelStepAarch64: *Step) []const u8 {
+//     b.installFile("build/copper.x86_64", "build/boot/x86_64/sys/copperkernel");
+//     b.installFile("build/copper.aarch64", "build/boot/aarch64/sys/copperkernel");
+//     b.installFile("bootboot/config", "config");
+
+//     const cwdpath: []u8 = fs.cwd().realpathAlloc(b.allocator, "zig-out") catch "";
+//     print("cwd:{s}\n", .{cwdpath});
+//     const mkimg = b.addSystemCommand(&[_][]const u8{ "../bootboot/mkbootimg", "../bootboot/copper-mkbootimg.json", "copper.img" });
+//     mkimg.cwd = cwdpath;
+//     b.getInstallStep().dependOn(&mkimg.step);
+
+//     // const mkiso = b.addSystemCommand(&[_][]const u8{ "grub-mkrescue", "-o", buildDir ++ "/copper.iso", buildDir ++ "/iso" });
+
+//     return "";
+
 //     // Copying the grub cfg for the iso
 //     //b.installFile("src/arch/x86/boot/multiboot_grub.cfg", "build/copperiso/boot/grub.cfg");
-//     b.installFile("src/arch/x86/boot/multiboot_grub.cfg", "iso/boot/grub.cfg");
+//     // b.installFile("src/arch/x86/boot/multiboot_grub.cfg", "iso/boot/grub.cfg");
 
-//     // Make multiboot iso
-//     const mkiso = b.addSystemCommand(&[_][]const u8{ "grub-mkrescue", "-o", buildDir ++ "/copper.iso", buildDir ++ "/iso" });
-//     mkiso.step.dependOn(kernelStep);
-//     b.default_step.dependOn(&mkiso.step);
+//     // // Make multiboot iso
+//     // const mkiso = b.addSystemCommand(&[_][]const u8{ "grub-mkrescue", "-o", buildDir ++ "/copper.iso", buildDir ++ "/iso" });
+//     // mkiso.step.dependOn(kernelStep);
+//     // b.default_step.dependOn(&mkiso.step);
 
-//     return buildDir ++ "/copper.iso";
+//     // return buildDir ++ "/copper.iso";
 // }
 
 // fn qemuStep(b: *Builder, imgPath: []const u8) void {
